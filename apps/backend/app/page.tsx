2fa48a98136874b2f60749a7b0feb269de27b0e2
omegamaster86 <masya868686@gmail.com>
2025-02-16 15:09:17 +0900

    命名変更

--- apps/backend/app/page.tsx
+++ apps/backend/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 
-export default function Home() {
+export default function Hono() {
   const [message, setMessage] = useState()
 
   useEffect(() => {
