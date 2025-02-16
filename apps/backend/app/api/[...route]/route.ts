31ceacb0af08ef53f7b4f6c36d74ab2f7728a74d
omegamaster86 <masya868686@gmail.com>
2025-02-16 23:07:45 +0900

    update

--- apps/backend/app/api/[...route]/route.ts
+++ apps/backend/app/api/[...route]/route.ts
@@ -6,6 +6,24 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
+let blog = [
+  {
+    id: 1,
+    title: 'Hello World',
+    body: 'Hello World',
+  },
+  {
+    id: 2,
+    title: 'Hello World2',
+    body: 'Hello World2',
+  },
+  {
+    id: 3,
+    title: 'Hello World3',
+    body: 'Hello World3',
+  },
+]
+
 app.use('/*' , 
   cors({
     origin: ['http://localhost:3001'],
